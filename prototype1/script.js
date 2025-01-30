57438585bc62d7cd344c7998050d30bd602b3a01
tomasg1655 <tomasgalvez1655@gmail.com>
2025-01-30 17:46:54 -0500

    Commit File Struct

--- prototype1/script.js
+++ prototype1/script.js
@@ -33,16 +33,17 @@ const sphereGeometry = new THREE.SphereGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
+const clock = new THREE.Clock()
 scene.add(testSphere)
 
 testSphere.position.set(0, 0, 0)
 
 
-const geometry = new THREE.TriangleGeometry (1.5, 1.5, 7)
-const material = new THREE.MeshNormalMaterial()
-const triangle = new THREE.Mesh(geometry,material)
-cube.position.set(0, 0.5, -5.5)
-scene.add(THREE.triangle)
+//const geometry = new THREE.TriangleGeometry (1.5, 1.5, 7)
+//const material = new THREE.MeshNormalMaterial()
+//const triangle = new THREE.Mesh(geometry,material)
+//cube.position.set(0, 0.5, -5.5)
+//scene.add(THREE.triangle)
 /********************
  ** ANIMATION LOOP **
 ********************/
@@ -52,12 +53,12 @@ const animation = () =>
 {
     // Return elasped Time
     const elapsedTime = clock.getElapsedTime ()
-
+    console.log(elapsedTime)
     // Animate testSphere
     testSphere.position.z = Math.sin(elapsedTime)
     const speed = 1
     const distance = 2
-    triangle.position.y = Math.sin(elapsedTime * speed) * distance
+    //triangle.position.y = Math.sin(elapsedTime * speed) * distance
     //Renderer
     renderer.render(scene, camera)
     // rotate
@@ -65,9 +66,9 @@ const animation = () =>
     
 
     //scale
-    triangle.scale.x = Math.sin(elapsedTime)
-    triangle.scale.y = Math.sin(elapsedTime)
-    triangle.scale.z = Math.sin(elapsedTime)
+    //triangle.scale.x = Math.sin(elapsedTime)
+   // triangle.scale.y = Math.sin(elapsedTime)
+   // triangle.scale.z = Math.sin(elapsedTime)
 
     //Request next frame
     window.requestAnimationFrame(animation)
